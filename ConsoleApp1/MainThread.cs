4a20b8bccdbc2048640ac1ec4079d51e424a5274
Giang Thị Tình <tinhgiang9898@gmail.com>
2018-05-31 09:36:14 +0700

    Tao thu muc C#

---
+++ ConsoleApp1/MainThread.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class MainThread
+    {
+        private static List<Student> list = new List<Student>();
+
+        public static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+
+        private static void SearchByName()
+        {
+            Console.WriteLine("Please enter name to seach: ");
+            var searchKey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[i];
+                if (student.Name == searchKey)
+                {
+                    Console.WriteLine("Found: ");
+                    Console.WriteLine(student.RollNumber + " - " + student.Name);
+                }
+            }
+        }
+
+        private static void AddStudent()
+        {
+            var student = new Student();
+            Console.WriteLine("Please enter student rollNumber:");
+            student.RollNumber = Console.ReadLine();
+            Console.WriteLine("Please enter student name:");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Please enter student email:");
+            student.Email = Console.ReadLine();
+            Console.WriteLine("Please enter student phone:");
+            student.Phone = Console.ReadLine();
+            list.Add(student);
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("============Student Menu===========");
+                Console.WriteLine("1. Nhập sinh viên.");
+                Console.WriteLine("2. In sinh viên ra List");
+                Console.WriteLine("3. Tìm sinh viên theo tên.");
+                Console.WriteLine("4. Thoát.");
+                Console.WriteLine("====================================");
+                Console.WriteLine("Mời bạn nhập số: ");
+                var choose = Int32.Parse(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayStudent();
+                        break;
+                    case 3:
+                        SearchByName();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+//        public static void Main(string[] args)
+//        {
+//            List<Student> list = new List<Student>();
+//            list.Add(new Student("A001", "Tình", "tinhgiang9898@gmail.com", "010423423"));
+//            list.Add(new Student("A002", "Tùng", "tung@gmail.com", "010423423"));
+//            list.Add(new Student("A003", "Sinh", "sinhnguyen@gmail.com", "010423423"));
+//
+//            foreach (var student in list)
+//            {
+//                Console.WriteLine(student.RollNumber);
+//                Console.WriteLine(student.Name);
+//                Console.WriteLine(student.Email);
+//                Console.WriteLine(student.Phone);
+//            }
+//        }
+    }
+}
